eff3f860eb83f75f4685e0a7d0a21ffe4920b38d
Forhad Uddin Ahmed <forhad.uddin892@gmail.com>
2024-12-17 18:59:26 +0600

    updated

--- payments/square_config.py
+++ payments/square_config.py
@@ -1,8 +1,8 @@
 from square.client import Client
 
 # Replace these with your Square Sandbox credentials
-SQUARE_ACCESS_TOKEN = "EAAAly5RUwCkpWuyX_Iv5-xmU2Zsjar3p7_YFeT3K7BSRyJqiq51B9R1XqnAnMCw"  # Access Token
-SQUARE_APPLICATION_ID = "sandbox-sq0idb-4zgLtGoIhnWJMIubsLWGTw"  # Application ID
+SQUARE_ACCESS_TOKEN = "_Iv5-xmU2Zsjar3p7_YFeT3K7BSRyJqiq51B9R1XqnAnMCw"  # Access Token
+SQUARE_APPLICATION_ID = "sq0idb-4zgLtGoIhnWJMIubsLWGTw"  # Application ID
 SQUARE_ENVIRONMENT = "sandbox"  # Use 'sandbox' or 'production'
 
 # Initialize the Square Client
